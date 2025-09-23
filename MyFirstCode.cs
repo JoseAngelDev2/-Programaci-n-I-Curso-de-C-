dcce563afc23342603197d517c5bc31476298e97
JoseAngelDev2 <casuhud1@gmail.com>
2025-09-23 12:26:52 -0400

    First Taks

---
+++ MyFirstCode.cs
@@ -0,0 +1,22 @@
+﻿
+int age = 14;
+string name = "Jose Angel";
+bool available = false;
+
+Console.WriteLine($"My name is: {name} // age: {age} // i have money: {available}");
+
+const int id = 1234;
+Console.WriteLine($"My id: {id}");
+
+int number = 4;
+int decreNumber = number - 2;
+int IncreNumber = number + 2;
+
+Console.WriteLine($"decreasing: {decreNumber} // increase:  {IncreNumber}");
+
+// Jose Angel Polanco, 20250971
+
+/*
+HELLO
+WORD
+*/
