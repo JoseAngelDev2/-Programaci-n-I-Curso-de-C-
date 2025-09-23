2eda40953fda550fed9d76c92d9ecf9c228c59fb
JoseAngelDev2 <casuhud1@gmail.com>
2025-09-23 12:51:17 -0400

    This Program calculates if it is even or odd

---
+++ TaksNumber2.cs
@@ -0,0 +1,16 @@
+﻿
+
+
+// this program calculates if it is even or odd
+
+Console.Write("Write a number: ");
+int number = int.Parse(Console.ReadLine()!);
+
+if (number % 2 == 0)
+{
+    Console.WriteLine($"The number {number} is even");
+}
+else
+{
+    Console.WriteLine($"The number {number} is odd");   
+}
